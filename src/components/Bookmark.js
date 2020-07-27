3c68b07a0dc9cfeb65446a4ae73b91e291aad7b0
amadi5892 <66728197+amadi5892@users.noreply.github.com>
2020-07-27 09:34:05 -0400

    Added Bookmark.js Profile.js to components folder

---
+++ src/components/Bookmark.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import axios from 'axios'
+
+class Bookmark extends React.Component {
+    constructor() {
+        super()
+
+        
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1>Bookmark</h1>
+            </div>
+        )
+    }
+}
+
+export default Bookmark
---
+++ src/components/Profile.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import axios from 'axios'
+
+class Profile extends React.Component {
+    constructor() {
+        super()
+
+
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1>Profile</h1>
+            </div>
+        )
+    }
+}
+
+export default Profile
