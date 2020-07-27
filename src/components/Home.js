a585523b6cbec67caaabbbd67ff4ecfda60da243
amadi5892 <66728197+amadi5892@users.noreply.github.com>
2020-07-27 09:46:54 -0400

    added Home.js to components

---
+++ src/components/Home.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const pawImages = [
+    'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1519742866993-66d3cfef4bbd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1488161628813-04466f872be2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+]
+
+export default class Home extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {
+            email: '',
+            password: '',
+            formCompleted: false
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Home</h1>
+            </div>
+        )
+    }
+}
