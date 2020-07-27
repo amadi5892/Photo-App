0d230247d4ac3070bee29f8c8114953b96d119c7
amadi5892 <66728197+amadi5892@users.noreply.github.com>
2020-07-27 19:42:42 -0400

    research communication

--- src/components/Home.js
+++ src/components/Home.js
@@ -21,6 +21,26 @@ export default class Home extends React.Component {
         }
     }
 
+    onEmailChange = (event) => {
+        this.setState({
+            email: event.target.value
+        })
+    }
+
+    onPasswordChange = (event) => {
+        this.setState({
+            password: event.target.value
+        })
+    }
+
+    handleFormSubmission = (e) => {
+        e.preventDefault()
+        this.setState({
+            formCompleted: true
+        })
+        console.log("Form is submitted")
+    }
+
     handleThumbClick(selectedImage) {
             this.setState({
             selectedImage
@@ -31,9 +51,49 @@ export default class Home extends React.Component {
         const { images, selectedImage } = this.state
         return (
             <div>
-                <h1>Home</h1>
-                <div >
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                Login
+                </button>
+
+                {/* <!-- Modal --> */}
+                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <form>
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Email address</label>
+                                <input type="email" value={this.state.email} onChange={this.onEmailChange} class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+                                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputPassword1">Password</label>
+                                <input type="password" value={this.state.password} onChange={this.onPasswordChange} class="form-control" id="exampleInputPassword1"></input>
+                            </div>
+                            <div class="form-group form-check">
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
+                                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                            </div>
+                            <button type="submit" onClick={this.handleFormSubmission} class="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                    </div>
+                </div>
+                </div>
+                    <div >
+                    {this.state.formCompleted === true ?(
                     <div className="container">
+                        <h1>Hello {this.state.email}</h1>
                         <div className="display-img" >
                             <div>
                                 <img src={selectedImage} alt=""></img>
@@ -47,6 +107,7 @@ export default class Home extends React.Component {
                             ))}
                         </div>
                     </div>
+                ) : null}
                 </div>
             </div>
         )
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import axios from 'axios'
+import React from 'react';
+import axios from 'axios';
+import Home from './Home';
 
 class Profile extends React.Component {
     constructor() {
