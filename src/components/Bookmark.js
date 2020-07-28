012d410b7ce707c6e449549229e643c08c38168b
amadi5892 <66728197+amadi5892@users.noreply.github.com>
2020-07-27 23:07:27 -0400

    filled in other pages momentarily

--- src/components/Bookmark.js
+++ src/components/Bookmark.js
@@ -1,17 +1,36 @@
 import React from 'react'
 import axios from 'axios'
 
-class Bookmark extends React.Component {
-    constructor() {
-        super()
+const pawImages = [
+    
+    'https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1519742866993-66d3cfef4bbd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    
+]
 
-        
+class Bookmark extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            images: pawImages,
+            
+        }
     }
     
     render() {
+        const {images} = this.state
         return (
             <div>
                 <h1>Bookmark</h1>
+                <div className="selection">
+                            {images.map((image, index) => (
+                                <div key={index}  >
+                                    <img src={image} alt=""></img>
+                                </div>
+                            ))}
+                        </div>
             </div>
         )
     }
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,18 +1,42 @@
-import React from 'react';
-import axios from 'axios';
-import Home from './Home';
+import React from 'react'
+
+
+
+const pawImages = [
+    'https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=653&q=80',
+    'https://images.unsplash.com/photo-1498837167922-ddd27525d352?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+    'https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1519742866993-66d3cfef4bbd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1541534741688-6078c6bfb5c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1049&q=80',
+    'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    
+]
 
 class Profile extends React.Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
 
-
+        this.state = {
+            images: pawImages,
+            
+        }
     }
     
     render() {
+        const {images} = this.state
         return (
             <div>
-                <h1>Profile</h1>
+                <h1>amadi5892@gmail.com</h1>
+                <img src="https://images.unsplash.com/photo-1483721310020-03333e577078?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1100&q=80" alt="" className="prof-pic"></img>
+                <div className="selection grid-block">
+                            {images.map((image, index) => (
+                                <div key={index}  >
+                                    <img src={image} alt="" className='one'></img>
+                                    <img src={image} alt="" className='one'></img>
+                                    <img src={image} alt="" className='one'></img>
+                                </div>
+                            ))}
+                        </div>
             </div>
         )
     }
